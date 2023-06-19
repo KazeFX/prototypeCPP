e499b7ccb8bc985ef2e8dc0258ab3c7e4490c6dd
KazeFX <kazefx@kaze.net>
2023-06-19 02:15:24 +0200

    Added function.

--- main.cpp
+++ main.cpp
@@ -1,7 +1,23 @@
 #include <iostream>
+using namespace std;
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+void tryingThis() {
+   int test[3][2] = {{2, -5},
+                      {4, 0},
+                      {9, 1}};
+
+    // use of nested for loop
+    // access rows of the array
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 2; ++j) {
+            cout << "test[" << i << "][" << j << "] = " << test[i][j] << endl;
+        }
+    }
 }
 
+int main() {
+    cout << "Hello, World!" << endl;
+    cout << "LOL" << endl;
+    tryingThis();
+    return 0;
+}
