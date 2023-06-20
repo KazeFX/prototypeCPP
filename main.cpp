4b9f0e70d1fa1b813c0cc634a2038b9ed84129e4
KazeFX <kazefx@kaze.net>
2023-06-20 03:24:23 +0200

    More functions and comments.

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-void tryingThis() {
-   int test[3][2] = {{2, -5},
+const char newline = '\n';
+#define PI 3.14159
+
+void test2dMatrix() {
+    cout << "== test2dMatrix ==" << endl;
+    int test[3][2] = {{2, -5},
                       {4, 0},
                       {9, 1}};
 
-    // use of nested for loop
-    // access rows of the array
     for (int i = 0; i < 3; ++i) {
         for (int j = 0; j < 2; ++j) {
             cout << "test[" << i << "][" << j << "] = " << test[i][j] << endl;
@@ -15,9 +18,32 @@ void tryingThis() {
     }
 }
 
+void testString() {
+    cout << "== testString ==" << endl;
+    string mystring;
+    mystring = "This is a string";
+    cout << mystring << endl;
+}
+
+void arith() {
+    cout << "== arith ==" << endl;
+    int a, b;
+    int result;
+
+    a = 5;
+    b = 2;
+    a = a + 1;
+    result = a - b;
+
+    cout << result << endl;
+}
+
 int main() {
-    cout << "Hello, World!" << endl;
-    cout << "LOL" << endl;
-    tryingThis();
+    cout << "Code Prototypes";
+    cout << newline << endl;
+    cout << PI << endl;
+    test2dMatrix();
+    testString();
+    arith();
     return 0;
 }
