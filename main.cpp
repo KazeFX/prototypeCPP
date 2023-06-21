f63fccf5fae22be7fb81f35ff6088bda0b1cc397
KazeFX <kaze@fx.net>
2023-06-21 03:50:13 +0200

    More code added.

--- main.cpp
+++ main.cpp
@@ -18,6 +18,23 @@ void test2dMatrix() {
     }
 }
 
+void inputTest() {
+    int i;
+    cout << "Please enter an integer value: ";
+    cin >> i;
+    cout << "The value you entered is " << i;
+    cout << " and its double is " << i*2 << endl;
+}
+
+void compareTest() {
+    cout << "== compareTest ==" << endl;
+    int a, b, c;
+    a = 2;
+    b = 7;
+    c = (a>b) ? a : b;
+    cout << c << endl;
+}
+
 void testString() {
     cout << "== testString ==" << endl;
     string mystring;
@@ -38,12 +55,25 @@ void arith() {
     cout << result << endl;
 }
 
+void getLineTest() {
+    string mystr;
+    cout << "What's your name? ";
+    getline (cin, mystr);
+    cout << "Hello " << mystr << ".\n";
+    cout << "What's your favorite team? ";
+    getline (cin, mystr);
+    cout << "I like " << mystr << " too!" << endl;
+}
+
 int main() {
-    cout << "Code Prototypes";
-    cout << newline << endl;
-    cout << PI << endl;
-    test2dMatrix();
-    testString();
-    arith();
+    //cout << "Code Prototypes";
+    //cout << newline << endl;
+    //cout << PI << endl;
+    //test2dMatrix();
+    //testString();
+    //arith();
+    //compareTest();
+    //inputTest();
+    //getLineTest();
     return 0;
 }
